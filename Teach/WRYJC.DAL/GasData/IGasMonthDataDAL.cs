74a268ce353e9c27dbbc1d788136bb83d400488b
DaiQing1995 <dq_199510@163.com>
2017-05-08 15:46:53 +0800

    Add other gas info

---
+++ Teach/WRYJC.DAL/GasData/IGasMonthDataDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRYJC.DAL.GasData
+{
+    public interface IGasMonthDataDAL
+    {
+    }
+}
